6cd1716d09bc4a8b1f92295b89d0ca8d395c0f6c
vladtarnovskiy <vladtarnovski989@gmail.com>
2022-08-08 01:50:56 +0300

    feat: translater start

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -11,7 +11,7 @@ function showTime () {
     time.textContent = currentTime
     setTimeout(showTime, 1000)
     showDate()
-    showGreeting()
+    
 }
 
 function showDate () {
@@ -22,8 +22,31 @@ function showDate () {
 }
 
 //Greeting
+const english = document.querySelector('.english')
+const russian = document.querySelector('.russian')
+
+const greetingTranslation = {
+    'en': {
+        'morning': 'Good morning',
+        'afternoon': 'Good afternoon',
+        'evening': 'Good evening',
+        'night': 'Good night',
+        'placeholder': '[Enter name]',
+        'localDate': 'en-US'
+    },
+    'ru': {
+        'morning': 'Доброе утро',
+        'afternoon': 'Добрый день',
+        'evening': 'Добрый вечер',
+        'night': 'Доброй ночи',
+        'placeholder': '[Введите имя]',
+        'localDate': 'ru'
+    }
+}
+
 
 const greeting = document.querySelector('.greeting')
+const name = document.querySelector('.name')
 
 function getTimeOfDay () {
     const date = new Date()
@@ -31,24 +54,32 @@ function getTimeOfDay () {
     let timeOfDay
 
     if (6 <=hours && hours< 12) {
-        timeOfDay = 'Morning'
+        timeOfDay = 'morning'
     } else if (12 <=hours && hours< 18) {
-        timeOfDay = 'Day'
+        timeOfDay = 'afternoon'
     } else if (18 <=hours && hours<= 24) {
-        timeOfDay = 'Evening'
+        timeOfDay = 'evening'
     } else if (0 <=hours && hours< 6) {
-        timeOfDay = 'Night'
+        timeOfDay = 'night'
     }
 
     return timeOfDay
 }
 
-function showGreeting() {
-    let greet = `Good ${getTimeOfDay()},`
+function showGreeting(lang = 'en') {
+    greeting.textContent = greetingTranslation[lang][getTimeOfDay()]
+    name.placeholder = greetingTranslation[lang]['placeholder']
 
-    greeting.textContent = greet
+    if (lang == 'ru'){
+        russian.classList.add('active');
+        english.classList.remove('active');
+    } else {
+        russian.classList.remove('active');
+        english.classList.add('active');
+    }
+      localStorage.setItem('lang', lang)
 }
-
+// запуск фнкции при загрузке???
 showTime()
 
 //Change background
@@ -56,7 +87,7 @@ showTime()
 const body = document.querySelector('body')
 const slidePrev = document.querySelector('.slide-prev')
 const slideNext = document.querySelector('.slide-next')
-let randomNum = getRandomNum(20)
+let randomNum = getRandomNum(19) + 1
 
 function getRandomNum (max) {
    return Math.floor(Math.random() * max)
@@ -67,9 +98,9 @@ function setBg () {
     let timeOfDay = getTimeOfDay().toLowerCase()
 
     const img = new Image();
-    img.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg`
+    img.src = `https://raw.githubusercontent.com/VladTarnovskiy/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg`
     img.onload = () => {      
-        body.style.backgroundImage = `url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg')`
+        body.style.backgroundImage = `url('https://raw.githubusercontent.com/VladTarnovskiy/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg')`
   }; 
 }
 
@@ -107,17 +138,21 @@ const city = document.querySelector('.city')
 const wind = document.querySelector('.wind')
 const humidity = document.querySelector('.humidity')
 
-async function getWeather () {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=en&appid=bb636cf0c1822af0ffa882199590a0dd&units=metric`
-    const res = await fetch(url)
-    const data = await res.json()
+async function getWeather (lang = 'en') {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=${lang}&appid=bb636cf0c1822af0ffa882199590a0dd&units=metric`
+    try{
+        const res = await fetch(url)
+        const data = await res.json()
 
-    weatherIcon.className = 'weather-icon owf';
-    weatherIcon.classList.add(`owf-${data.weather[0].id}`);
-    temperature.textContent = `${data.main.temp}°C`;
-    weatherDescription.textContent = data.weather[0].description;
-    wind.textContent = `Wind speed: ${data.wind.speed}m/s`;
-    humidity.textContent = `Humidity: ${data.main.humidity}%`;
+        weatherIcon.className = 'weather-icon owf';
+        weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+        temperature.textContent = `${Math.round(data.main.temp)}°C`;
+        weatherDescription.textContent = data.weather[0].description;
+        wind.textContent = `Wind speed: ${Math.round(data.wind.speed)}m/s`;
+        humidity.textContent = `Humidity: ${Math.round(data.main.humidity)}%`;
+    } catch(err) {
+        alert('The city doesnt exist!')
+    }
 }
 
 getWeather()
@@ -127,6 +162,17 @@ city.addEventListener('change', () => {
     getWeather()
 })
 
+
+russian.addEventListener('click', () => {
+showGreeting('ru');
+getWeather('ru')
+} 
+)
+english.addEventListener('click', () => {
+    showGreeting('en');
+    getWeather('en')
+}
+)
 //Quote
 
 const quote = document.querySelector('.quote')
@@ -134,10 +180,10 @@ const author = document.querySelector('.author')
 const buttonChangeQuote = document.querySelector('.change-quote')
 
 async function getQuote () {
-    const url = `/momentum/data/data.json`
+    const url = `/js/data.json`
     const res = await fetch(url)
     const data = await res.json()
-    const randomQuoteNumber = getRandomNum(3)
+    const randomQuoteNumber = getRandomNum(data.length)
     quote.textContent = `${data[randomQuoteNumber].text}`;
     author.textContent = `${data[randomQuoteNumber].author}`;
 }
@@ -154,6 +200,8 @@ import playList from './playList.js'
 const play = document.querySelector('.play')
 const butNextAudio = document.querySelector('.play-next')
 const butPrevAudio = document.querySelector('.play-prev')
+const playerList = document.querySelector('.play-list')
+const playerControls = document.querySelector('.player-controls')
 let isPlay = false
 let playNum = 0
 const audio = new Audio();
@@ -161,43 +209,92 @@ const audio = new Audio();
 function playAudio () {
     audio.src = playList[playNum].src
     audio.currentTime = 0;
-    if (isPlay == false){
-        play.classList.toggle('pause')
-        isPlay = true
-        audio.play()
-    } else {
-        play.classList.toggle('pause')
+    play.classList.add('pause')
+    isPlay = true
+    audio.play()
+    
+}
+
+function pauseAudio (){
+    if (isPlay == true){
+        play.classList.remove('pause')
         isPlay = false
         audio.pause();
     }
 }
-play.addEventListener('click', playAudio)
+
+play.addEventListener('click', () => {
+    if (isPlay == false){
+        playAudio()
+    } else {
+        pauseAudio()
+    }
+})
 
 function playNext (){
-    playNum += 1
+    if (playNum < 3){
+        playNum += 1
+    } else {
+        playNum = 0
+    }
     playAudio()
 }
 butNextAudio.addEventListener('click', playNext)
+audio.addEventListener('ended', playNext)
+// При окончании трека глючит маркер
 
 function playPrev (){
-    playNum -= 1
+    if (playNum > 0){
+        playNum -= 1
+    } else {
+        playNum = 3
+    }
     playAudio()
 }
 butPrevAudio.addEventListener('click', playPrev)
 
+playList.forEach((item, index) => {
+    const li = document.createElement('li')
+    li.classList.add('play-item')
+    li.textContent = playList[index].title
+    playerControls.addEventListener('click', () => {
+        if (index == playNum){
+            li.classList.add('item-active')
+        } else {
+            li.classList.remove('item-active')
+        }
+    })
+    
+    playerList.append(li)
+})
+
+//Translater
+
 
 
 //Local Storage
-const name = document.querySelector('.name')
 
 function setLocalStorage() {
     localStorage.setItem('name', name.value);
+    localStorage.setItem('city', city.value);
   }
-  window.addEventListener('beforeunload', setLocalStorage)
 
-  function getLocalStorage() {
+window.addEventListener('beforeunload', setLocalStorage)
+
+function getLocalStorage() {
+    let lang = localStorage.getItem('lang')
+
     if(localStorage.getItem('name')) {
-      name.value = localStorage.getItem('name');
+        name.value = localStorage.getItem('name');
     }
-  }
-  window.addEventListener('load', getLocalStorage)
+
+    if(localStorage.getItem('city')) {
+        city.value = localStorage.getItem('city');
+    }
+
+    showGreeting(lang)
+    getWeather(lang)
+
+}
+
+window.addEventListener('load', getLocalStorage)
